d01c6c1f7fd4bb1ce752b0e3431581fb8ac9b588
Waqas Shaikh <farazshaikh7837@gmail.com>
2024-02-07 12:05:38 +0500

    files add

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Header from './componet/Header/Header'
+import Footer from './componet/Footer/Footer'
+import './App.css'
+function App() {
+  
+
+  return (
+    <>
+    
+    <Header></Header>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
